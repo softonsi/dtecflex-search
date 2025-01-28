d0cc9623547736dcdbf04b19c5e5997c7fd31eb2
sandro-softon <sandro@softon.com.br>
2025-01-28 10:45:54 -0300

    fix: ajuste do script de para ficar portavel

--- start_app.sh
+++ start_app.sh
@@ -10,5 +10,4 @@ mkdir -p ./log
 
 /home/softon/.local/bin/uv venv
 source .venv/bin/activate
-
-/home/softon/.local/bin/uv run streamlit run app.py --server.port 8502 >> ./log/app.log 2>&1
+$UV run streamlit run app.py --server.port 8502 >> "$LOG" 2>&1
