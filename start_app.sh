9a2afb82ff49ce806bd1487d3401bf0cdcd0f730
sandro-softon <sandro@softon.com.br>
2025-01-28 10:50:19 -0300

    fix: ajuste do start_app.py para ficar portavel

--- start_app.sh
+++ start_app.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 # Obtém o diretório do script
-script_dir="$(dirname "$(readlink -f "$0")")"
-cd $script_dir
 
-echo "$script_dir"
+DIR="$(dirname "$(readlink -f "$0")")"
+cd $DIR
 
-mkdir -p ./log
+UV=$(which uv)
+DATA=$(date '+%Y-%m-%d_%H-%M-%S')
+LOG="$DIR/log/app_$DATA.log"
 
-/home/softon/.local/bin/uv venv
+echo "DIR: $(pwd)"
+echo "LOG: $LOG"
+echo "UV : $UV"
+
+mkdir -p "$DIR/log"
+
+$UV venv
 source .venv/bin/activate
 $UV run streamlit run app.py --server.port 8502 >> "$LOG" 2>&1
